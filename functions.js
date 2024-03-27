6021ea328820ce1cb3f1c40aa1fe411a851b3051
Demchyk Dmytro <d.demchyk.im21@kpi.ua>
2024-03-27 13:28:23 +0200

    converter basic realization
    fill convertMarkdown function
    todo: add error handling

--- functions.js
+++ functions.js
@@ -1,1 +1,62 @@
-export const convertMarkdown = (text) => {};
+const replacements = [
+    { from: /```(.*?)```/gs, to: '<pre>$1</pre>' }, // preformatted
+    { from: /(?:^|\n)(.*?)\n/g, to: '<p>$1</p>' }, // paragraph
+    { 
+        from: /(?<![\dA-Za-zА-Яа-я])\*\*([\dA-Za-zА-Яа-я])(.*?)([\dA-Za-zА-Яа-я])\*\*(?![\dA-Za-zА-Яа-я])/,
+        to: '<i>$1$2$3</i>', // bold
+    },
+    { 
+        from: /(?<![\dA-Za-zА-Яа-я])_([\dA-Za-zА-Яа-я])(.*?)([\dA-Za-zА-Яа-я])_(?![\dA-Za-zА-Яа-я])/,
+        to: '<i>$1$2$3</i>', // italic
+    },
+    { 
+        from: /(?<![\dA-Za-zА-Яа-я])`([\dA-Za-zА-Яа-я])(.*?)([\dA-Za-zА-Яа-я])`(?![\dA-Za-zА-Яа-я])/,
+        to: '<i>$1$2$3</i>', // monospaced
+    },
+];
+
+export const convertMarkdown = (text) => {
+    const {replaced, preformattedBlocks } = replacePreformatedWithToken(text);
+    let html = replaced;
+
+    replacements.slice(1).forEach(replacement => {
+        html = html.replace(replacement.from, replacement.to);
+    });
+
+    html = replaceTokenWithPreformatted(html, preformattedBlocks);
+
+    return html;
+};
+
+const replacePreformatedWithToken = (text) => {
+    const preformattedBlocks = [];
+    const token = '%%PRE%%';
+
+    const replaced = text.replace(replacements[0].from, (match, group) => {
+        preformattedBlocks.push(group);
+        return token;
+    });
+
+    return {
+        replaced,
+        preformattedBlocks,
+    }
+}
+
+const replaceTokenWithPreformatted = (text, preformattedBlocks) => {
+    return text.replace(/%%PRE%%/g, () => {
+        return `<pre>${preformattedBlocks.shift()}</pre>`;
+    });
+}
+
+const checkForNestedTags = (text) => {
+    tegRegexp = /<(\w+)>(.*)<\/\1>/g;
+
+    const matches = text.matchAll(tegRegexp);
+    for (const match of matches) {
+        const [fullMatch, tag, content] = match;
+        if (content.match(tegRegexp)) {
+            return true;
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { Command } from 'commander';
-import { convertMarkdown } from './functions';
+import { convertMarkdown } from './functions.js';
 
 const program = new Command();
 
 program
     .command('convert <file>')
     .description('Convert a markdown file to HTML')
-    .argument('<file>', 'The markdown file to convert')
     .option('-o, --out <file>', 'The output file')
     .action((file, options) => {
         const markdown = readFileSync(file, 'utf-8');
         const html = convertMarkdown(markdown);
-        
+
         const writeFunc = options.out ? writeFileSync.bind(null, options.out) : console.log;
 
         writeFunc(html);
     });
+
+program.parse(process.argv);
